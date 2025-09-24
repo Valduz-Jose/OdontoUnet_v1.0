4be8dc183bc633091cebe64b68958bb2b7111599
Jose Valduz <jose.valduz@unet.edu.ve>
2025-09-24 12:56:34 -0400

    para que corra en vercel

--- src/app.js
+++ src/app.js
@@ -1,33 +1,36 @@
-import express from 'express'
-import morgan from 'morgan'
-import authRoutes from './routes/auth.routes.js'
-import cookieParser from 'cookie-parser'
-import patientsRoutes from './routes/patients.routes.js'
-import citasRoutes from "./routes/citas.routes.js"
+import express from "express";
+import morgan from "morgan";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import patientsRoutes from "./routes/patients.routes.js";
+import citasRoutes from "./routes/citas.routes.js";
 import insumoRoutes from "./routes/insumos.routes.js";
-import citaInsumoRoutes from "./routes/citaInsumos.routes.js"
-import profileRoutes from "./routes/profile.routes.js"
-import carouselRoutes from "./routes/carousel.routes.js"
-import statisticsRoutes from "./routes/statistics.routes.js"
-import cors from 'cors'
-import path from 'path'
+import citaInsumoRoutes from "./routes/citaInsumos.routes.js";
+import profileRoutes from "./routes/profile.routes.js";
+import carouselRoutes from "./routes/carousel.routes.js";
+import statisticsRoutes from "./routes/statistics.routes.js";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials: true
-}));
-app.use(morgan('dev'));
+app.use(
+  cors({
+    // origin:'http://localhost:5173',
+    origin: "https://odontounet-frontend.vercel.app",
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
 // Servir archivos estáticos
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // Rutas de la API
-app.use("/api",authRoutes);
-app.use("/api",patientsRoutes);
+app.use("/api", authRoutes);
+app.use("/api", patientsRoutes);
 app.use("/api", citasRoutes);
 app.use("/api", insumoRoutes);
 app.use("/api", citaInsumoRoutes);
