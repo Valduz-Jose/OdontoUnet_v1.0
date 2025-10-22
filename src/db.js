01db9898ad4fffd46e3bab7892a69a3e5e9f8a24
Jose Valduz <jose.valduz@unet.edu.ve>
2025-10-22 13:27:28 -0400

    buscando conexion con render

--- src/db.js
+++ src/db.js
@@ -1,10 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import { MONGODB_URI } from "./config.js";
 
-export const connectDB = async ()=>{
-    try {
-        await mongoose.connect('mongodb://localhost/odontoUnet');
-        console.log(">>> DB is Conected");
-    } catch (error) {
-        console.log(error);
-    }
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log(">>> DB is Connected");
+  } catch (error) {
+    console.error("Error conectando a MongoDB:", error);
+    process.exit(1); // Termina el proceso si no puede conectar
+  }
+};
